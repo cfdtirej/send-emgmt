4b6b0231bc7922d74aba244f91dfa6e84c81437c
cfdtirej <fkueee5113@gmail.com>
2020-08-07 16:58:22 +0900

    get diff, type conv

--- client/client.py
+++ client/client.py
@@ -1,25 +1,43 @@
 import socket
 
-import read_csv
-import config
+import client.read_csv as read_csv
+import client.config as config
+
 
 class SendEmgmt:
-    def __init__(self, host, port, measurement):
+    def __init__(self, host, port):
         self.host = host
         self.port = port
-        self.measurement = measurement
 
-    def send(self, table):
+    def send(self, latest_file, prev_file):
+        csv_diff = read_csv.get_csv_diff(latest_file, prev_file) # dictionary type
+        column = csv_diff[0][1:]
+        time = []
+        for i in range(len(csv_diff)):
+            time.append(csv_diff[i][0])
+        count = 0
         while True:
-            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as socket:
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client:
+                influx_json = [
+                    {
+                        'measurement': 'measurement',
+                        'tags': {
+                            'type': 'tag value'
+                        },
+                        'time': time[count],
+                        'fields': dict(zip(column, csv_diff[count][1:]))
+                    }
+                ]
                 msg = 'msg'
-                s.connect((self.host, self.port))
-                s.sendall(msg.encode('utf-8'))
-                recv_msg = s.recv(4096)
-                print(recv_msg + '\n ##############################################')
+                client.connect((self.host, self.port))
+                client.sendall(msg.encode('utf-8'))
+                recv_msg = client.recv(4096)
+                print('#---Server message---#\n{}'.format(recv_msg))
+                count += 1
+                if count == len(csv_diff):
+                    break
 
 
 if __name__ == '__main__':
     host = config.host
     port = config.port
-    measurement = config.
--- client/read_csv.py
+++ client/read_csv.py
@@ -17,19 +17,42 @@ def check_charset(filepath):
 
 # Get file diff array
 def get_csv_diff(latest_file, prev_file):
+    # using 'check_charset' function
     charset = check_charset(latest_file)
     with open(latest_file, 'r', encoding=charset) as f:
         reader = csv.reader(f)
         latest_table = [row for row in reader]
+        if 'hpcs' in latest_file:
+            column = latest_table[0]
+            del latest_table[0]
+        else:
+            column = latest_table[2]
+            del latest_table[:2]
     with open(prev_file, 'r', encoding=charset) as f:
         reader = csv.reader(f)
         prev_table = [row for row in reader]
+        if 'hpcs' in prev_file:
+            del prev_table[0]
+        else:
+            del prev_table[:2]
+    # get latest to previous diff two dim array
     for prev in prev_table:
         for latest in latest_table:
             if latest == prev:
                 latest_table.remove(latest)
+    # type conversion
+    for i in range(len(latest_table)):
+        for j in range(len(latest_table[0])):
+            try:
+                latest_table[i][j] = float(latest_table[i][j])
+            except ValueError:
+                latest_table[i][j] = str(latest_table[i][j])
+    latest_table.insert(0, column)
     return latest_table
 
 
 if __name__ == '__main__':
-    print(get_csv_diff('log_data/dc.CSV', 'prev_log/dc.CSV'))
+    # print(get_csv_diff('log_data/dc.csv', 'prev_log/dc.csv'))
+    a = get_csv_diff('log_data/dc.csv', 'prev_log/dc.csv')
+    print(a)
+    print(type(a[0][1]), type(a[0][0]))
